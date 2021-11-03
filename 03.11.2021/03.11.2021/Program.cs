010e0fe6ee3899309c35280be558d95965a7f95d
OmarOsmanov <tu201906019@code.edu.az>
2021-11-03 17:58:04 +0400

    first commit

---
+++ 03.11.2021/03.11.2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Yaratdigim_class_library;
+
+namespace _03._11._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryninIcindeYaratdigim library = new LibraryninIcindeYaratdigim();
+            Console.WriteLine(library.add(4.6M, 5.6M));
+            Console.WriteLine(library.ferqi(6.789M, 2.456M));
+            Console.WriteLine(library.power(8, 3));
+            Console.WriteLine(library.UcbucaginSahesi(6, 5));
+        }
+    }
+}
---
+++ 03.11.2021/Yaratdigim class library/LibraryninIcindeYaratdigim.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Yaratdigim_class_library
+{
+    public class LibraryninIcindeYaratdigim
+    {
+
+        public int add(int a, int b)
+        {
+            int total = (a * a * a) + (b * b * b);
+            return total;
+        }
+        public decimal add(decimal a, decimal b)
+        {
+            decimal total = (a * a * a) + (b * b * b);
+            return total;
+        }
+        public decimal ferqi(decimal a, decimal b)
+        {
+            decimal total = a - b;
+            return total;
+        }
+
+        public int power(int a, byte b)
+        {
+            int pow = 1;
+            for (int i = 0; i < b; i++)
+            {
+                pow *= b;
+            }
+            return pow;
+        }
+
+        public int UcbucaginSahesi(int a,int h)
+        {
+            int total = (a * h) / 2;
+            return total;
+        }
+
+    }
+
+}
+
